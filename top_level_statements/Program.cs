0b80124981274804e80cf1f98f80542248bc1778
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-29 16:28:28 +0100

    Lesson 1. Top level statements

---
+++ top_level_statements/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using My;
+var name = "";
+while (String.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("What is your name?");
+    name = Console.ReadLine();
+}
+var person = new Person(name);
+var webAddress = "";
+while (String.IsNullOrWhiteSpace(webAddress) || !webAddress.StartsWith("http"))
+{
+    Console.WriteLine("What is your desired webaddress?");
+    webAddress = Console.ReadLine();
+}
+Print("Hi");
+Console.WriteLine(await new HttpClient().GetAsync(webAddress));
+
+void Print(string text) => Console.WriteLine(text + " " + person.Name + "!");
+
+namespace My
+{
+    public class Person
+    {
+        public string Name { get; }
+        public Person(string name) => Name = name;
+    }
+}
