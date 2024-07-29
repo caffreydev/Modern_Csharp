dbe85189e8a1a662fb5ed73c9b5802f53010ad15
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-29 16:29:08 +0100

    Lesson 3 Using Declarations

---
+++ using_declarations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        WriteLines("file.txt", new[] { "Line 1", "Line 2" });
+    }
+
+
+    public static void WriteLines(string fileName, string[] lines)
+    {
+        using var fileStream = new FileStream(fileName, FileMode.Create);
+        using var streamWriter = new StreamWriter(fileStream);
+        foreach (var line in lines)
+        {
+            streamWriter.WriteLine(line);
+        }
+    }
+}
