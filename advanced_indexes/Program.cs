09071ce750c3056153bb6276d5cc3cdb531a783c
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-30 17:03:49 +0100

    Lesson 9 Advanced Indexes

---
+++ advanced_indexes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var text = "0123456789";
+        var numbers = new List<int> { 10, 20, 30, 40, 50 };
+
+        int n = 1;
+        Console.WriteLine(text[^n]);
+        Console.WriteLine(numbers[^(n + 2)]);
+    }
+}
