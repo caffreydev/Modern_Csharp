c2ef749b9f43cb4ff44f7208fbcd0421655e915a
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-30 17:02:08 +0100

    Lesson 5 raw string literals

---
+++ raw_string_literals/Program.cs
@@ -0,0 +1,21 @@
+﻿var code = """"
+var title = "What's new in C# 11?";
+
+var json = $$"""
+    [
+        {
+            "title": "{{title}}",
+            "url": "https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-11"
+        },
+
+        {
+            "title": "What's new in C# 10?",
+            "url": "https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-10"
+        }
+    ]
+    """;
+
+WriteLine(json);
+"""";
+
+WriteLine(code);
