1d003448127b9f804d639ed570e903cfce31e13a
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-30 17:03:38 +0100

    Lesson 8 Tuples

---
+++ tuples/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+static (int Sum, int Product) Calculate(int x, int y)
+{
+    return (x + y, x * y);
+}
+
+int x = 2;
+int y = 3;
+(int sum, int product) = Calculate(x, y);
+
+Console.WriteLine($"x is : {x}, y is : {y}, sum is {sum}, product is {product}");
