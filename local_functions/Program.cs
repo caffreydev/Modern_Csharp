b996b8f0e361504ebe52921cfd5ce25e2c3f206b
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-30 17:04:11 +0100

    Lesson 11 Local Functions

---
+++ local_functions/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string name = "";
+        while (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Enter your name:");
+            name = Console.ReadLine();
+        }
+        Intro(name);
+    }
+
+    public static void Intro(string name)
+    {
+        Console.WriteLine($"Hi {FormatName()}, how's it going?");
+
+        string FormatName()
+        {
+            if (name.Contains(' '))
+            {
+                return name.Split(' ')[0];
+            }
+            else
+            {
+                return name;
+            }
+        }
+    }
+}
