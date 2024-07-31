d399cfff4a76c07b7844dc251b73cb9e75920095
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-31 10:20:51 +0100

    Lesson 15 Async main

---
+++ async_main/Program.cs
@@ -0,0 +1,10 @@
+﻿
+public class Program
+{
+    public static async Task Main()
+    {
+        var result = await new HttpClient().GetStringAsync("http://www.andreaangella.com");
+
+        Console.WriteLine(result[0..100]);
+    }
+}
