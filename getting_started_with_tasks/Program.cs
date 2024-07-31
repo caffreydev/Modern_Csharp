b45e9c1c40cf6e81bdafe3d6f4f0d329d82a3528
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-31 10:47:31 +0100

    Lesson 16 Getting Started with tasks

---
+++ getting_started_with_tasks/Program.cs
@@ -0,0 +1,13 @@
+﻿
+for (var i = 1; i <= 10; i++)
+{
+    Task.Factory.StartNew(n =>
+    {
+        Console.WriteLine($"Starting Task {n}");
+        Thread.Sleep(1000); // Long running operation
+        Console.WriteLine($"Ended Task {n}");
+    }, i);
+
+
+    Console.ReadKey();
+}
