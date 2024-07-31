a41a420cdf8475316e7f387b2a03834ee3161dda
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-31 11:13:20 +0100

    Lesson 17 How to call a rest API in C#

---
+++ getting_started_with_tasks/calling_a_rest_api/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+using (var httpClient = new HttpClient() { BaseAddress = new Uri("https://api.github.com") })
+{
+    httpClient.DefaultRequestHeaders.Add("User-Agent", "caffreydev");
+    var response = await httpClient.GetAsync("/orgs/microsoft/repos");
+    var content = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(response.StatusCode);
+    Console.WriteLine(content[1..1000]);
+
+    var json = JsonConvert.DeserializeObject(content);
+
+    Console.WriteLine(json);
+}
