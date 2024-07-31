99f009bfdbbe56ce64f7eab18bbbcf8e89412e20
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-31 10:10:13 +0100

    Lesson 13 Switch Expressions

---
+++ switch_expressions/Program.cs
@@ -0,0 +1,16 @@
+﻿
+PrintState(true);
+PrintState(false);
+PrintState(null);
+
+static void PrintState(bool? state)
+{
+    var message = state switch
+    {
+        true => "State is true",
+        false => "State is false",
+        _ => "State is indeterminate"
+    };
+
+    Console.WriteLine(message);
+}
