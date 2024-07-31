165e10a2a5219c5059590a9d61bca7791556c246
Joe McCaffrey <joe.mccaffrey@mmtdigital.co.uk>
2024-07-31 10:10:25 +0100

    Lesson 14 Expression Body Members

---
+++ expression_body_members/Program.cs
@@ -0,0 +1,35 @@
+﻿
+var money = new Money(10);
+Console.WriteLine(money);
+try
+{
+    money.Value = -100;
+}
+catch (Exception)
+{
+    Console.WriteLine("Can't set the value to a  negative number.");
+}
+
+money.Value = 100;
+
+Console.WriteLine(money);
+
+class Money
+{
+    private decimal value;
+
+    public decimal Value
+    {
+        get => value;
+        set => SetValue(value);
+    }
+
+    public Money(decimal value) => SetValue(value);
+
+    private void SetValue(decimal newValue) => value = newValue >= 0 ? newValue : throw new Exception();
+
+    public string AsString => "$" + value;
+
+    public override string ToString() => $"Your money is {AsString}";
+}
+
